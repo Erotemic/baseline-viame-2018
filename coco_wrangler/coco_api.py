37b1f041b2a63e59bfbd083a4af4d0d51efd0f01
Jon Crall <jon.crall@kitware.com>
2018-03-08 09:32:16 -0500

    wip

--- coco_wrangler/coco_api.py
+++ coco_wrangler/coco_api.py
@@ -186,7 +186,7 @@ class CocoDataset(ub.NiceRepr):
         self.cid_to_gids = ub.map_vals(sorted, cid_to_gids)
         self.cid_to_aids = ub.map_vals(sorted, cid_to_aids)
 
-        self.name_to_cat = {cat['name']: cid for cid, cat in self.cats.items()}
+        self.name_to_cat = {cat['name']: cat for cat in self.cats.values()}
 
     @classmethod
     def union(cls, *others):
